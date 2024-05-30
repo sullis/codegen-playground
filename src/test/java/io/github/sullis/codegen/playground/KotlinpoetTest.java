196aea14c2de26831abf9d2cf6e7fdc8f62d2025
sullis <github@seansullivan.com>
2024-05-30 07:50:33 -0700

    add rxjava2

--- src/test/java/io/github/sullis/codegen/playground/KotlinpoetTest.java
+++ src/test/java/io/github/sullis/codegen/playground/KotlinpoetTest.java
@@ -1,5 +1,6 @@
 package io.github.sullis.codegen.playground;
 
+import io.reactivex.Single;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +14,12 @@ public class KotlinpoetTest {
   void happyPath() {
     ClassName className = new ClassName("foobar", List.of("Hello"));
     assertThat(className.toString()).isEqualTo("foobar.Hello");
+  }
+
+  @Test
+  void happyPath_rxjava_Single() {
+    Class<?> clazz = Single.class;
+    ClassName className = new ClassName(clazz.getPackageName(), clazz.getSimpleName());
+    assertThat(className.toString()).isEqualTo("io.reactivex.Single");
   }
 }
