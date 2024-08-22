a40166fbe49480615d0c3c71a78cbe8717971cd4
sullis <github@seansullivan.com>
2024-08-22 09:48:26 -0700

    add MicronautSourcegenTest.java

---
+++ src/test/java/io/github/sullis/codegen/playground/MicronautSourcegenTest.java
@@ -0,0 +1,15 @@
+package io.github.sullis.codegen.playground;
+
+import org.junit.jupiter.api.Test;
+
+public class MicronautSourcegenTest {
+  @Test
+  void testJava() {
+    // TODO
+  }
+
+  @Test
+  void testKotlin() {
+    // TODO
+  }
+}
